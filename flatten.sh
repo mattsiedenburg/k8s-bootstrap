c1db9fd4dd1c44604d319a22202441c5296e6683
Matthew Siedenburg <matthewsiedenburg@protonmail.com>
2021-02-09 00:59:34 +0000

    Updates to bootstrap scripts

--- flatten.sh
+++ flatten.sh
@@ -54,10 +54,14 @@ iptables -P OUTPUT ACCEPT
 iptables -P FORWARD ACCEPT
 
 
-systemctl start docker
-systemctl start kubelet
-
 apt-mark unhold docker-ce-cli docker-ce kubelet kubeadm kubectl
-apt autoremove -y --allow-change-held-packages docker-ce kubelet kubeadm kubectl docker-ce docker-ce-cli
+apt-get autoremove -y --purge --allow-change-held-packages docker-ce kubelet kubeadm kubectl docker-ce docker-ce-cli containerd.io
 
+add-apt-repository -r "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+rm -rf /etc/apt/sources.list.d/helm-stable-debian.list*
+rm -rf /etc/apt/sources.list.d/kubernetes.list*
 
+sudo apt-key del "9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88"
+sudo apt-key del "81BF 832E 2F19 CD2A A047  1959 294A C482 7C1A 168A"
+sudo apt-key del "54A6 47F9 048D 5688 D7DA  2ABE 6A03 0B21 BA07 F4FB"
+sudo apt-key del "59FE 0256 8272 69DC 8157  8F92 8B57 C5C2 836F 4BEB"
--- install.sh
+++ install.sh
@@ -4,9 +4,29 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-apt autoremove -y --allow-change-held-packages docker-ce kubelet kubeadm kubectl docker-ce docker-ce-cli
-apt install -y --allow-change-held-packages docker-ce=5:19.03.11~3-0~ubuntu-bionic docker-ce-cli=5:19.03.11~3-0~ubuntu-bionic kubelet=1.19.7-00 kubeadm=1.19.7-00 kubectl=1.19.7-00
-apt-mark hold docker-ce docker-ce-cli kubelet kubeadm kubectl
+apt-get autoremove -y --purge --allow-change-held-packages docker-ce kubelet kubeadm kubectl docker-ce docker-ce-cli docker docker-engine docker.io containerd runc
+apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+
+apt-get update
+
+apt-get install -y --allow-change-held-packages docker-ce=5:19.03.11~3-0~ubuntu-bionic docker-ce-cli=5:19.03.11~3-0~ubuntu-bionic kubelet=1.19.7-00 kubeadm=1.19.7-00 kubectl=1.19.7-00 containerd.io helm
+apt-mark hold docker-ce docker-ce-cli kubelet kubeadm kubectl containerd.io helm
+
+usermod -aG docker $(logname)
 
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 br_netfilter
@@ -24,7 +44,7 @@ kubeadm init --control-plane-endpoint=$(hostname) --pod-network-cidr=10.244.0.0/
 
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown -R $(logname):$(logname) $HOME/.kube
+chown -R $(logname):$(id $(logname) -gn) $HOME/.kube
 
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
