888963d267a24cbd5270e784bac9d97fe00d15c5
Matt Siedenburg <mattsiedenburg@protonmail.com>
2021-03-01 16:08:24 -0800

    Improving structure

--- imperative_install.sh
+++ imperative_install.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+sudo apt-get autoremove -y docker docker-engine docker.io containerd runc
+
 sudo apt-get install \
     apt-transport-https \
     ca-certificates \
@@ -14,7 +18,10 @@ sudo add-apt-repository \
    
 sudo apt-get update
 
-sudo apt-get install docker-ce=5:19.03.15~3-0~ubuntu-focal docker-ce-cli=5:19.03.15~3-0~ubuntu-focal containerd.io -y
+sudo apt-get install -y \
+    docker-ce=5:19.03.15~3-0~ubuntu-focal \
+    docker-ce-cli=5:19.03.15~3-0~ubuntu-focal \
+    containerd.io=1.2.13-2
 
 sudo apt-mark hold docker-ce docker-ce-cli containerd.io
 
@@ -28,7 +35,6 @@ net.bridge.bridge-nf-call-iptables = 1
 EOF
 sudo sysctl --system
 
-sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
@@ -37,9 +43,6 @@ sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
-/boot/firmware/cmdline.txt
-append to first line
-cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1
-reboot
+sudo sed -i '$ s/$/ cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 swapaccount=1/' /boot/firmware/cmdline.txt
 
-kubeadm init --control-plane-endpoint=$(hostname) --pod-network-cidr=10.244.0.0/16
+sudo reboot
